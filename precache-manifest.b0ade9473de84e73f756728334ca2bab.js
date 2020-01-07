a1cfe7fe380eeca2261267df409c58ca3bf7744b
olivia@ruiz-knott.com <olivia@ruiz-knott.com>
2020-01-07 01:30:14 -0500

    Updates

---
+++ precache-manifest.b0ade9473de84e73f756728334ca2bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2259a007003176821667fa9572c3fe0f",
+    "url": "/guestbook2019/index.html"
+  },
+  {
+    "revision": "ec4660a365f3a9da8c72",
+    "url": "/guestbook2019/static/css/main.507e26e2.chunk.css"
+  },
+  {
+    "revision": "a0c2bc4ac29fdb4e4bd9",
+    "url": "/guestbook2019/static/js/2.b2739d15.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/guestbook2019/static/js/2.b2739d15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec4660a365f3a9da8c72",
+    "url": "/guestbook2019/static/js/main.7af6aa50.chunk.js"
+  },
+  {
+    "revision": "ee630fbf3af13e92a8c2",
+    "url": "/guestbook2019/static/js/runtime-main.225b077e.js"
+  }
+]);
