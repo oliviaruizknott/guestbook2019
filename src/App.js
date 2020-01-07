6ee89aa58eb821142d4f1b7824d873fc6e6a2fa7
olivia@ruiz-knott.com <olivia@ruiz-knott.com>
2020-01-07 00:39:47 -0500

    Styling and put Router back

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import DataViz2019 from './DataViz2019';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HashRouter basename="/">
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={DataViz2019} />
           <Route path='/:name' component={DataViz2019} />
-        </HashRouter>
-      </div>
+        </div>
+      </Router>
     )
   }
 }
